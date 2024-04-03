761c7d72f8a97a45c042736537ee6d07cd407a00
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-04-03 19:01:03 +0530

    privateKey moved to .env and Making it backend readt

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  "expense_tracker_react",
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   { dialect: "mysql", host: process.env.DB_HOST }
